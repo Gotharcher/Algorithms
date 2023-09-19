9f5b2350bec021541b42f35393345964d9557442
Gotharcher <a.chepik@crystal-sound.com>
2023-09-19 15:35:22 +0300

    4 - WA. Fixed TL by stepped out from iterated sum to prefix-sum in Maps. (from 6s *TL*/50MB -> 4s/180Mb WA) Seems to be somewhere int/long loses.

--- src/main/java/Yandex/Example_2023_4.java
+++ src/main/java/Yandex/Example_2023_4.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class Example_2023_4 {
     public static Map<Integer, Integer> pricesMap = new HashMap<>();
     public static Map<Integer, Integer> lengthMap = new HashMap<>();
+
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] ars) throws IOException {
@@ -22,53 +23,63 @@ public class Example_2023_4 {
             pricesMap.put(startTime, pricesMap.getOrDefault(startTime, 0) + Integer.parseInt(orderData[2]));
             lengthMap.put(stopTime, lengthMap.getOrDefault(stopTime, 0) + stopTime-startTime);
         }
-        List<Integer> listOfStarts = new ArrayList<>(pricesMap.keySet());
-        Collections.sort(listOfStarts);
-
-        List<Integer> listOfEnds = new ArrayList<>(lengthMap.keySet());
-        Collections.sort(listOfEnds);
-
         int questionsQty = Integer.parseInt(br.readLine());
+        int[][] questionsArray= new int[questionsQty][3];
         for (int i = 0; i < questionsQty; i++) {
-            long res = 0;
             String[] questions = br.readLine().split(" ");
             int startTime = Integer.parseInt(questions[0]);
             int stopTime = Integer.parseInt(questions[1]);
-            if(questions[2].equals("1")){
-                List<Integer> resList = getSubarrayOfValues(listOfStarts, startTime, stopTime);
-                for(Integer k: resList){
-                    res += pricesMap.get(k);
-                }
+            int question = Integer.parseInt(questions[2]);
+
+            if(question == 1) {
+                pricesMap.putIfAbsent(startTime-1, 0);
+                pricesMap.putIfAbsent(stopTime, 0);
             }
-            if(questions[2].equals("2")){
-                List<Integer> resList = getSubarrayOfValues(listOfEnds, startTime, stopTime);
-                for(Integer k: resList){
-                    res += lengthMap.get(k);
-                }
+            if(question == 2) {
+                lengthMap.putIfAbsent(startTime-1, 0);
+                lengthMap.putIfAbsent(stopTime, 0);
             }
-            sb.append(res);
+
+            questionsArray[i][0] = startTime;
+            questionsArray[i][1] = stopTime;
+            questionsArray[i][2] = question;
+        }
+
+        List<Integer> listOfStarts = new ArrayList<>(pricesMap.keySet());
+        Collections.sort(listOfStarts);
+        Map<Integer, Long>  rsqPrices = getPrefixesMap(listOfStarts, pricesMap);
+
+        List<Integer> listOfEnds = new ArrayList<>(lengthMap.keySet());
+        Collections.sort(listOfEnds);
+        Map<Integer, Long>  rsqLengths = getPrefixesMap(listOfEnds, lengthMap);
+
+        for(int k=0; k<questionsQty; k++){
+            long res = 0;
+            if(questionsArray[k][2] == 1){
+                res = countPrefSumm(rsqPrices, questionsArray[k][0] - 1, questionsArray[k][1]);
+                sb.append(res);
+
+            }
+            if(questionsArray[k][2]==2){
+                res = countPrefSumm(rsqLengths, questionsArray[k][0] - 1, questionsArray[k][1]);
+                sb.append(res);
+            }
             sb.append(" ");
         }
         System.out.println(sb);
     }
 
-    public static List<Integer> getSubarrayOfValues(List<Integer> arr, int start, int stop){
-        int startIndex =-1, stopIndex = -1;
-        for(int i=0; i<arr.size(); i++ ){
-            if(arr.get(i) >= start){
-                startIndex = i;
-                break;
-            }
+    public static Map<Integer, Long> getPrefixesMap(List<Integer> initList, Map<Integer, Integer> sourceMap){
+        Map<Integer, Long> prefMap = new HashMap<>();
+        prefMap.put(0, 0L);
+        prefMap.put(initList.get(0), 0L);
+        for(int i=1; i<initList.size(); i++){
+            prefMap.put(initList.get(i), prefMap.get(initList.get(i-1)) + sourceMap.get(initList.get(i)));
         }
-        for(int i=arr.size()-1; i>=0; i--){
-            if(arr.get(i) <= stop){
-                stopIndex = i;
-                break;
-            }
-        }
-        if(startIndex < 0 || stopIndex < 0){
-            return new ArrayList<>();
-        }
-        return arr.subList(startIndex, stopIndex+1);
+        return prefMap;
+    }
+
+    public static long countPrefSumm(Map<Integer, Long> prefMap, int startVal, int stopVal){
+        return prefMap.get(stopVal) - prefMap.get(startVal);
     }
 }
