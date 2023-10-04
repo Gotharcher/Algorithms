a65765130fb1350b8b94365fe01731bc2c1faa6d
Gotharcher <a.chepik@crystal-sound.com>
2023-10-04 11:52:48 +0300

    Exclude Math import in python, changed to pow-check. Task passed.

--- src/main/java/Yandex/Algo_course/Sprint1/1J.py
+++ src/main/java/Yandex/Algo_course/Sprint1/1J.py
@@ -1,17 +1,17 @@
-import math
-
 a = int(input())
 ans = []
 
 def find_divider(val):
-    for n in range(2, math.isqrt(a)+1):
+    n = 2
+    while n * n <= a:
         if val%n == 0:
             return n, val//n
+        n = n+1
     return val, 1
 
 while(a>1):
-    n, a = find_divider(a)
-    if(n>1):
-        ans.append(n)
+    k, a = find_divider(a)
+    if(k>1):
+        ans.append(k)
 
 print(*ans)
