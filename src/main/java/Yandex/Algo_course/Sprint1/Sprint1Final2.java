286fe14a09c31422e80f25c6d291d488a0670832
Arseny Chepik <firecat.brew@yandex.ru>
2023-10-08 18:55:20 +0300

    Fixed review notes for Sprint 1 finals.

--- src/main/java/Yandex/Algo_course/Sprint1/Sprint1Final2.java
+++ src/main/java/Yandex/Algo_course/Sprint1/Sprint1Final2.java
@@ -9,25 +9,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Sprint1Final2 {
+    public static final int DATA_ROWS = 4;
+    public static final int GAME_CYCLES = 9;
+    public static final int PLAYERS_COUNT = 2;
     public static Map<Integer, Integer> digitsCount = new HashMap<>();
     //нет требований к положению "рук", поэтому просто сравним число пальцев (которыми оперируют игроки) с числом кнопок
 
     public static void main(String[] ars) throws IOException {
         int ans = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int maxInput = Integer.parseInt(br.readLine())*2;
-        for(int i=1; i<=4; i++){ //4 строки с данными
+        int maxInput = Integer.parseInt(br.readLine()) * PLAYERS_COUNT;
+        for (int i = 1; i <= DATA_ROWS; i++) { //4 строки с данными
             stringToMap(br.readLine());
         }
-        for(int i=1; i<=9; i++){
-            if(digitsCount.get(i) != null && digitsCount.get(i) <= maxInput){
+        for (int i = 1; i <= GAME_CYCLES; i++) {
+            if (digitsCount.get(i) != null && digitsCount.get(i) <= maxInput) {
                 ans++;
             }
         }
         System.out.println(ans);
     }
 
-    public static void stringToMap(String s){
+    public static void stringToMap(String s) {
         char[] ca = s.toCharArray();
         for (char c : ca) {
             if (c == '.') {
