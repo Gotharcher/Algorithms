ea109abc023bff1f8de7fbd627a9787076e66778
Gotharcher <sauarhagg@list.ru>
2023-11-02 12:26:25 +0300

    Intro E. Solved.

--- src/main/java/Yandex/algos4/intro/IntroE.java
+++ src/main/java/Yandex/algos4/intro/IntroE.java
@@ -3,8 +3,6 @@ package Yandex.algos4.intro;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 //Идея - так как по неубыванию, "левые" результаты будут идти от 0 к минус беск (1-1, 1-3, 1-4), а правые к 0 справа (4-1, 4-3, 4-4).
@@ -16,27 +14,24 @@ public class IntroE {
         int studs = Integer.parseInt(br.readLine());
         int[] rating = new int[studs];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<studs;i++){
+        for (int i = 0; i < studs; i++) {
             rating[i] = Integer.parseInt(st.nextToken());
-        }
-        Map<Integer, Integer> ratingsMap = new HashMap<>();
-
-        for(int i = 0; i<studs;i++){
-            if(ratingsMap.containsKey(rating[i])){
-                continue;
-            }else{
-                int sum = 0;
-                for(int j = 0; j < studs; j++){
-                    sum += Math.abs(rating[i] - rating[j]);
-                    ratingsMap.put(rating[i], sum);
-                }
-            }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<studs;i++){
-            sb.append(ratingsMap.get(rating[i])).append(" ");
+        int leftleftsum = 0;
+        for (int i = 0; i < studs; i++) {
+            leftleftsum += Math.abs(rating[0] - rating[i]);
         }
+        sb.append(leftleftsum).append(" ");
+
+        for (int i = 1; i < studs; i++) {
+            int dt = rating[i] - rating[i - 1];
+            leftleftsum += (dt * i) - (dt * (studs - i));
+
+            sb.append(leftleftsum).append(" ");
+        }
+
         System.out.println(sb);
     }
 }
